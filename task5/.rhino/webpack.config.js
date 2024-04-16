5349d9324977be1691c62eef6258a97f7b5ca67b
Kamil Zyla <kamil@appsilon.com>
2024-04-16 18:27:17 +0200

    feat(task5): Git-track .rhino

---
+++ task5/.rhino/webpack.config.js
@@ -0,0 +1,24 @@
+const { join } = require('path');
+
+const appDir = join(__dirname, '..', 'app');
+
+module.exports = {
+  mode: 'production',
+  entry: join(appDir, 'js', 'index.js'),
+  output: {
+    library: 'App',
+    path: join(appDir, 'static', 'js'),
+    filename: 'app.min.js',
+  },
+  module: {
+    rules: [
+      {
+        test: /\.(js|jsx)$/,
+        use: 'babel-loader',
+      },
+    ],
+  },
+  resolve: {
+    extensions: ['.js', '.jsx'],
+  },
+};
