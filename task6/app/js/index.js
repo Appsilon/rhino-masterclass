65701b08d564629673c603b3976f384a4a4fb912
Kamil Zyla <kamil@appsilon.com>
2024-04-17 00:23:21 +0200

    feat(task6): Custom hello

--- task6/app/js/index.js
+++ task6/app/js/index.js
@@ -1,8 +1,10 @@
-export function message(text) {
-  window.alert(text);
+export function sayHello(name) {
+  window.alert(`Hello ${name}!`);
 }
 
-export function shortString(data, type, row, meta) {
+export function shortColumn(data, type, row, meta) {
 return type === 'display' && data.length > 6 ?
 '<span title=\"' + data + '\">' + data.substr(0, 6) + '...</span>' : data;
 }
+
+Shiny.addCustomMessageHandler('sayHello', sayHello);
--- task6/app/main.R
+++ task6/app/main.R
@@ -8,7 +8,7 @@ iris_table <- datatable(
   data = iris[90:109, ],
   options = list(
     columnDefs = list(
-      list(targets = 5, render = JS("App.shortString"))
+      list(targets = 5, render = JS("App.shortColumn"))
     )
   )
 )
@@ -18,15 +18,20 @@ ui <- function(id) {
   ns <- shiny$NS(id)
   shiny$fluidPage(
     tags$h1("Hello"),
-    tags$button("Say Hello!", onclick = "App.message('Hello!')"),
+    tags$button("Say Hello", onclick = "App.sayHello('Rhino')"),
     tags$h1("Iris table"),
-    iris_table
+    iris_table,
+    tags$h1("Custom hello"),
+    shiny$textInput(ns("name"), "What is your name?"),
+    shiny$actionButton(ns("hello"), "Say Hello")
   )
 }
 
 #' @export
 server <- function(id) {
   shiny$moduleServer(id, function(input, output, session) {
-
+    shiny$observe({
+      session$sendCustomMessage("sayHello", input$name)
+    }) |> shiny$bindEvent(input$hello)
   })
 }
