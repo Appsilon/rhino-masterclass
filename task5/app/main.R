5959c8d8e51d4f3a90d70ca52e63ad068bf15333
Kamil Zyla <kamil@appsilon.com>
2024-04-16 17:50:02 +0200

    feat(task5): Check if app greets user with their name

--- task5/app/main.R
+++ task5/app/main.R
@@ -7,12 +7,14 @@ ui <- function(id) {
   ns <- shiny$NS(id)
   shiny$fluidPage(
     shiny$titlePanel("Rhino App"),
+    shiny$textInput(ns("name"), "What is your name?"),
+    shiny$textOutput(ns("text"))
   )
 }
 
 #' @export
 server <- function(id) {
   shiny$moduleServer(id, function(input, output, session) {
-
+    output$text <- shiny$renderText(paste0("Hello ", shiny$req(input$name), "!"))
   })
 }
--- task5/tests/cypress/e2e/app.cy.js
+++ task5/tests/cypress/e2e/app.cy.js
@@ -1,9 +1,16 @@
 describe('app', () => {
   beforeEach(() => {
-    cy.visit('/')
-  })
+    cy.visit('/');
+  });
 
   it('displays a title', () => {
-    cy.contains("Rhino App")
-  })
-})
+    cy.contains('Rhino App');
+  });
+
+  it('greets user with their name', () => {
+    cy.get('#app-name')
+      .type('Rhino');
+    cy.get('#app-text')
+      .contains('Hello Rhino!');
+  });
+});
