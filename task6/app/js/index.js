dab99c99390689d066f5c122cfc652a1e5105bae
Kamil Zyla <kamil@appsilon.com>
2024-04-17 00:00:10 +0200

    feat(task6): Iris table with custom column rendering

--- task6/app/js/index.js
+++ task6/app/js/index.js
@@ -1,3 +1,8 @@
 export function message(text) {
   window.alert(text);
 }
+
+export function shortString(data, type, row, meta) {
+return type === 'display' && data.length > 6 ?
+'<span title=\"' + data + '\">' + data.substr(0, 6) + '...</span>' : data;
+}
--- task6/app/main.R
+++ task6/app/main.R
@@ -1,13 +1,26 @@
 box::use(
-  shiny,
-  shiny[tags],
+  datasets[iris],
+  DT[datatable, DTOutput, JS, renderDT],
+  shiny = shiny[tags],
+)
+
+iris_table <- datatable(
+  data = iris[90:109, ],
+  options = list(
+    columnDefs = list(
+      list(targets = 5, render = JS("App.shortString"))
+    )
+  )
 )
 
 #' @export
 ui <- function(id) {
   ns <- shiny$NS(id)
-  shiny$bootstrapPage(
+  shiny$fluidPage(
+    tags$h1("Hello"),
     tags$button("Say Hello!", onclick = "App.message('Hello!')"),
+    tags$h1("Iris table"),
+    iris_table
   )
 }
 
