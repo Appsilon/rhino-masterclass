44e863c4694d67d732cab1a05f2c3e2d63696288
Kamil Zyla <kamil@appsilon.com>
2024-04-17 01:00:40 +0200

    feat(task4): Hello message with center class

--- task4/app/main.R
+++ task4/app/main.R
@@ -1,25 +1,22 @@
 box::use(
-  shiny[bootstrapPage, div, moduleServer, NS, renderUI, tags, uiOutput],
+  shiny,
+  shiny[tags],
 )
 
 #' @export
 ui <- function(id) {
-  ns <- NS(id)
-  bootstrapPage(
-    uiOutput(ns("message"))
+  ns <- shiny$NS(id)
+  shiny$fluidPage(
+    shiny$div(
+      class = "center",
+      tags$h1("Hello!")
+    )
   )
 }
 
 #' @export
 server <- function(id) {
-  moduleServer(id, function(input, output, session) {
-    output$message <- renderUI({
-      div(
-        style = "display: flex; justify-content: center; align-items: center; height: 100vh;",
-        tags$h1(
-          tags$a("Check out Rhino docs!", href = "https://appsilon.github.io/rhino/")
-        )
-      )
-    })
+  shiny$moduleServer(id, function(input, output, session) {
+
   })
 }
