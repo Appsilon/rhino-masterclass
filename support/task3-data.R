07ccb30acb3068cb638eb915a8c30749a65fe3c6
Kamil Zyla <kamil@appsilon.com>
2024-04-17 13:07:05 +0200

    feat(task3): Make group an integer and add DATA folder

--- support/task3-data.R
+++ support/task3-data.R
@@ -2,7 +2,7 @@ fruits <- c("apples", "bananas", "cherries")
 vegetables <- c("cucumbers", "onions", "potatoes", "tomatoes")
 
 favorites <- data.frame(
-  group = c(rep(1, 25), rep(2, 25), rep(3, 25), rep(4, 25)),
+  group = c(rep(1L, 25), rep(2L, 25), rep(3L, 25), rep(4L, 25)),
   age = sample(20:70, 100, replace = TRUE),
   fruit = sample(fruits, 100, replace = TRUE),
   vegetable = sample(vegetables, 100, replace = TRUE)
